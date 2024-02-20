1a9bfc5e468ac8c44cf3891f4eee67a464d2a7b8
Sanya__Petruk <oleksandr.petruk@infopulse.com>
2024-02-20 17:44:20 +0200

    update(view page): create styles

--- src/app/admin/[id]/page.tsx
+++ src/app/admin/[id]/page.tsx
@@ -18,50 +18,92 @@ export default function View({params: {id}}: Props){
 
     }, []);
     return(
-        <main>
-            <p>Як до вас можна звертатися?</p>
-            <p>{brif?.nameOfCustomer}</p>
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
-            <p>Як ми можемо з вами зв'язатися?</p>
-            <p>{brif?.contacts}</p>
-            <p>Яка назва вашої компанії?</p>
-            <p>{brif?.nameOfCompany}</p>
-            <p>Яка назва вашого продукту?</p>
-            <p>{brif?.nameOfProduct}</p>
-            <p>Проєкт вже існує?</p>
-            <p>{brif?.isProjectExist ? "Так" : "Ні"}</p>
-            <p>Проєкт має більше однієї мови?</p>
-            <p>{brif?.isMoreOneLanguage ? "Так" : "Ні"}</p>
-            <p>Чи потрібно вам SEO оптимізація?</p>
-            <p>{brif?.isNeedSEO ? "Так" : "Ні"}</p>
-            <p>Чи потрібна вам підримка проєкту?</p>
-            <p>{brif?.isNeedSupport ? "Так" : "Ні"}</p>
-            <p>Чи потрібен вам дизайн?</p>
-            <p>{brif?.isYouHaveDesign ? "Так" : "Ні"}</p>
-            <p>Чи у вас є бренд проєкту?</p>
-            <p>{brif?.isYouHaveBrand ? "Так" : "Ні"}</p>
-            <p>Це комерційний проєкт?</p>
-            <p>{brif?.isCommerceProject ? "Так" : "Ні"}</p>
-            <p>Якою сферою діяльності ви займаєтесь?</p>
-            <p>{brif?.Activity}</p>
-            <p>Який тип проєкту у вас?</p>
-            <p>{brif?.typeOfProject}</p>
-            <p>Посилання на сайт конкурентів?</p>
-            <p>{brif?.link}</p>
-            <p>Що вам подобається на сайті?</p>
-            <p>{brif?.like}</p>
-            <p>Що вам не подобається на сайті?</p>
-            <p>{brif?.dislike}</p>
-            <p>Який вік вашої цільової аудиторії?</p>
-            <p>{brif?.auditoryInfo}</p>
-            <p>Який тип дизайну ви хочете?</p>
-            <p>{brif?.typeOfDesign}</p>
-            <p>Дата початку роботи</p>
-            <p>{brif?.dayOfStart}</p>
-            <p>Дата кінця роботи</p>
-            <p>{brif?.deadLine}</p>
-            <p>Очікувана сума затрат на проєкт</p>
-            <p>{brif?.sumOfMoney}</p>
+        <main style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Як до вас можна звертатися? -</p>
+                <p>{brif?.nameOfCustomer}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                <p>Як ми можемо з вами зв'язатися? -</p>
+                <p>{brif?.contacts}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Яка назва вашої компанії? -</p>
+                <p>{brif?.nameOfCompany}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Яка назва вашого продукту? -</p>
+                <p>{brif?.nameOfProduct}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Проєкт вже існує? -</p>
+                <p>{brif?.isProjectExist ? "Так" : "Ні"}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Проєкт має більше однієї мови? -</p>
+                <p>{brif?.isMoreOneLanguage ? "Так" : "Ні"}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Чи потрібно вам SEO оптимізація? -</p>
+                <p>{brif?.isNeedSEO ? "Так" : "Ні"}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Чи потрібна вам підримка проєкту? -</p>
+                <p>{brif?.isNeedSupport ? "Так" : "Ні"}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Чи потрібен вам дизайн? -</p>
+                <p>{brif?.isYouHaveDesign ? "Так" : "Ні"}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Чи у вас є бренд проєкту? -</p>
+                <p>{brif?.isYouHaveBrand ? "Так" : "Ні"}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Це комерційний проєкт? -</p>
+                <p>{brif?.isCommerceProject ? "Так" : "Ні"}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Якою сферою діяльності ви займаєтесь? -</p>
+                <p>{brif?.Activity}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Який тип проєкту у вас? -</p>
+                <p>{brif?.typeOfProject}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Посилання на сайт конкурентів? -</p>
+                <p>{brif?.link}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Що вам подобається на сайті? -</p>
+                <p>{brif?.like}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Що вам не подобається на сайті? -</p>
+                <p>{brif?.dislike}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Який вік вашої цільової аудиторії? -</p>
+                <p>{brif?.auditoryInfo}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Який тип дизайну ви хочете? -</p>
+                <p>{brif?.typeOfDesign}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Дата початку роботи -</p>
+                <p>{brif?.dayOfStart}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Дата кінця роботи -</p>
+                <p>{brif?.deadLine}</p>
+            </div>
+            <div style={{display: 'flex', gap: 5}}>
+                <p>Очікувана сума затрат на проєкт -</p>
+                <p>{brif?.sumOfMoney}</p>
+            </div>
         </main>
-    )
+)
 }
